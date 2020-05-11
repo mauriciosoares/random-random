03f590e913f14ee090eaa83084a26b4e05d97b76
Mauricio Soares <m.soares2603@gmail.com>
2020-05-11 19:37:19 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.924293e0d1e42f049ced6e826e5f5298.js"
+  "/precache-manifest.0228a5a0bd26a2b246a6c2dea6dfb4a5.js"
 );
 
 self.addEventListener('message', (event) => {
