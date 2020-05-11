59739bdf97344106059d42766a8875c7e148b45c
Mauricio Soares <m.soares2603@gmail.com>
2020-05-11 18:37:53 -0300

    Updates

---
+++ precache-manifest.924293e0d1e42f049ced6e826e5f5298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4b724397833c52551cab0c15af3462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe323556bde9bd2388f4",
+    "url": "/static/css/main.3cba87f7.chunk.css"
+  },
+  {
+    "revision": "4c170726f6889993f0dd",
+    "url": "/static/js/2.ff4edd67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ff4edd67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe323556bde9bd2388f4",
+    "url": "/static/js/main.d528fd67.chunk.js"
+  },
+  {
+    "revision": "1e7adf728aa55cea0318",
+    "url": "/static/js/runtime-main.3d1e67a6.js"
+  }
+]);
